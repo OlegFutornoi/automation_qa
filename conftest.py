7000430a3a67aa3f41c2ee197d5a8614fec75bec
Oleg Futornoi <oleg.futornoi@gmail.com>
2024-10-17 10:12:36 +0300

    Shablon project

--- conftest.py
+++ conftest.py
@@ -1,12 +1,23 @@
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope="function")
 def driver():
     """Повертає екземпляр драйвера Chrome для тестування."""
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    chrome_options = Options()
+    # chrome_options.add_argument("--windows-size=1920,1080")
+
+    # Налаштування драйвера через Service
+    service = Service(ChromeDriverManager().install())
+
+
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.maximize_window()
     yield driver
-    driver.quit()
+    driver.quit()  # Закриває браузер після тесту
+
+
